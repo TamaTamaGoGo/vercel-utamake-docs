1c230f0d375c099b7d0581975ba6950b9c05661b
TamaGoes <tamatama@tensi.org>
2021-07-07 19:56:23 +0900

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,9 +3,9 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">UtamakeWiki</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        Official pages of Utamake World
       </span>
     </>
   ),
